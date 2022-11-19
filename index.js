550d67983099d295813009f79e445f76d1227b19
Mary Ellison Barlow <maryellisonbarlow@gmail.com>
2022-11-18 21:53:45 -0500

    commit tree 802cd595c5ed8e1452e3d1a9fa11f38e71839a53

--- index.js
+++ index.js
@@ -1,9 +1,64 @@
+// on optimization:
+// this solution is much better on performance and memory because the other
+// solution would create new arrays and iterate through them with a bunch of
+// js methods. But spliting hairs - both methods will be very good.
+
 function isPalindrome(word) {
   // Write your algorithm here
+  // iterate from the beginning to the middle
+  // racecar 7 / 2 == 3.5
+  // aaaaaa 6 / 3 == 3
+  for (let i = 0; i < word.length / 2; i++) {
+//   check each letter to the corresponding letter from the end
+    const j = word.length - 1 - i
+//    const startChar = word[i]
+//    const endChar = word[j]
+//   if any of the letters don't match, return false, otherwise return true
+//    if (startChar !== endChar) return false;
+// code above be refractored without the variables of startChar and endChar
+// it can be understood through the code if written out like below
+    if (word[i] !== word[j]) return false
+    return true;
+/* 
+r a c e c a r
+0 1 2 3 4 5 6
+i           j
+
+r a c e c a r
+0 1 2 3 4 5 6
+  i       j
+
+r a c e c a r
+0 1 2 3 4 5 6
+    i   j
+
+r a c e c a r
+0 1 2 3 4 5 6
+      ij
+*/
+  }
 }
 
 /* 
   Add your pseudocode here
+  the problem:
+  abba
+  ^  ^
+   ^^
+   true
+  
+  robot
+  ^   ^
+   false 
+  make a function that returns true if the word is a palindrome. That means if the
+  first letter is the same as the last letter, and the second letter is the same as
+  the penultimate letter, etc. until we hit the middle, return true.
+
+  pseudocode:
+  iterate from the beginning to the middle
+    check each letter to the corresponding letter from the end
+        if any of the letters don't match, return false
+    return true
 */
 
 /*
